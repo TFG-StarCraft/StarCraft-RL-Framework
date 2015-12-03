9126c1f4a4817a1bd44184671d23bcf0772c35d7
mascanio <mascanio13@gmail.com>
2015-12-03 20:47:42 +0100

    synchronized use on ActionDispatchQueue

--- src/bot/action/ActionDispatchQueue.java
+++ src/bot/action/ActionDispatchQueue.java
@@ -1,7 +1,6 @@
 package bot.action;
 
 import java.util.ArrayList;
-import java.util.concurrent.Semaphore;
 
 import com.Com;
 
@@ -10,46 +9,28 @@ import qLearning.agent.Action;
 public class ActionDispatchQueue {
 
 	private Com com;
-	
+
 	private ArrayList<Action> list;
-	private Semaphore sem;
-	
+
 	public ActionDispatchQueue(Com com) {
 		this.com = com;
-		
+
 		this.list = new ArrayList<>();
-		
-		this.sem = new Semaphore(1);
 	}
 
-	public void put(Action action) {
-		try {
-			this.sem.acquire();
-		} catch (InterruptedException e) {
-			com.onError(e.getLocalizedMessage(), true);
-		}
-		
+	public synchronized void put(Action action) {
 		this.list.add(action);
-		
-		this.sem.release();
 	}
-	
-	public ArrayList<GenericAction> getQueue() {
-		try {
-			this.sem.acquire();
-		} catch (InterruptedException e) {
-			com.onError(e.getLocalizedMessage(), true);
-		}
-		
+
+	public synchronized ArrayList<GenericAction> getQueue() {
+
 		ArrayList<GenericAction> r = new ArrayList<GenericAction>();
 		for (Action action : list) {
 			r.add(action.toAction(com));
 		}
-		
-		this.list.clear();		
-		this.sem.release();
-		
+
+		this.list.clear();
 		return r;
 	}
-	
+
 }
