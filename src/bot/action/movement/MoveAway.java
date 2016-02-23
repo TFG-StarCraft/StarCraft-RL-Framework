2d4a3ab438cbf3b0f63f46449a410d0853a31dd3
Albert <albertllamuks@gmail.com>
2016-02-23 20:05:35 +0100

    Update MoveAway.java

--- src/bot/action/movement/MoveAway.java
+++ src/bot/action/movement/MoveAway.java
@@ -8,38 +8,39 @@ import bwapi.Unit;
 import bwapi.UnitType;
 import bwapi.WeaponType;
 
+/**
+ * Movement. Moving away from a target unit.
+ * @author Alberto Casas Ortiz.
+ * @author Raúl Martín Guadaño
+ * @author Miguel Ascanio Gómez.
+ */
 public class MoveAway extends MoveAction{
-
+	
+	
+	/***************/
+	/* CONSTRUCTOR */
+	/***************/
+	
+	/**
+	 * Constructor of the class MoveAway.
+	 * @param com Comunication.
+	 * @param unit Unit to move.
+	 * @param agentEpoch 
+	 */
 	public MoveAway(Com com, Unit unit, int agentEpoch) {
 		super(com, unit, agentEpoch);
 	}
 
-	@Override
-	protected void setUpMove() {
-		List<Unit> l = getGroundUnitsInRange();
-		if (!l.isEmpty()) {
-			Unit u = l.get(0);
-			double vX = u.getX() - unit.getX();
-			double vY = u.getY() - unit.getY();
-			double modulo = Math.sqrt(vX*vX + vY*vY);
-			vX /= modulo;
-			vY /= modulo;
-			
-			this.endX = unit.getX() + (int) Math.ceil(-vX * bot.Const.STEP);
-			this.endY = unit.getY() + (int) Math.ceil(-vY * bot.Const.STEP);
-			
-			
-
-		}
-		//Si no, no hacemos nada
-		else{
-			this.endX = unit.getX();
-			this.endY = unit.getY();
-		}
-	}
-
-
 	
+	
+	/******************/
+	/* PRIVATE METHOD */
+	/******************/
+	
+	/**
+	 * Get the units in the range of the moving unit.
+	 * @return A list with the units in the range.
+	 */
 	private List<Unit> getGroundUnitsInRange() {
 		
 		Unit u = this.unit;
@@ -48,4 +49,42 @@ public class MoveAway extends MoveAction{
 		
 		return this.unit.getUnitsInRadius(w.maxRange());
 	}
+
+	
+	
+	/*******************/
+	/* OVERRIDE METHOD */
+	/*******************/
+	
+	/**
+	 * Do the approach movement.
+	 */
+	@Override
+	protected void setUpMove() {
+		List<Unit> l = getGroundUnitsInRange();
+		if (!l.isEmpty()) {
+			Unit u = l.get(0);
+			//Calculate vector from unit to target.
+			double vX = u.getX() - unit.getX();
+			double vY = u.getY() - unit.getY();
+			double modulo = Math.sqrt(vX*vX + vY*vY);
+			vX /= modulo;
+			vY /= modulo;
+			
+			//Advance step from target to target.
+			this.endX = unit.getX() + (int) Math.ceil(-vX * bot.Const.STEP);
+			this.endY = unit.getY() + (int) Math.ceil(-vY * bot.Const.STEP);
+			
+			
+
+		}
+		//Otherwise, do nothing.
+		else{
+			this.endX = unit.getX();
+			this.endY = unit.getY();
+		}
+	}
+
+	
+	
 }
