ca07765323b5b54650c84af2556aa1ab4e28bbbd
Miguel Ascanio Gómez <mascanio13@gmail.com>
2016-02-23 12:25:16 +0100

    Change name of .bat lnk
    Changed from forceClose.bat - Acceso directo.lnk to 
    	forceClose.bat.lnk

--- src/gui/GUI.java
+++ src/gui/GUI.java
@@ -98,7 +98,7 @@ public class GUI extends JFrame {
 		if (!a.contains("Chaoslauncher.lnk") || !a.contains("forceClose.bat - Acceso directo.lnk")) {
 			System.err.println("ERROR: se necesita (en el directorio de ejecución) los siguientes accesos directos:\n"
 					+ "\tChaoslauncher.lnk que apunte a Chaoslauncher.exe, CON PRIVILEGIOS DE ADMINISTRADOR\n"
-					+ "\tforceClose.bat - Acceso directo.lnk que apunte al forceClose.bat de este directorio,\n"
+					+ "\tforceClose.bat.lnk que apunte al forceClose.bat de este directorio,\n"
 					+ "\t\tCON PRIVILEGIOS DE ADMINISTRADOR");
 		} else {
 
